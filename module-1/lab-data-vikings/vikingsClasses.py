db0263cb96a0e1ea1a7c68391a6415b87955f70f
miriamvelloso <62302676+miriamvelloso@users.noreply.github.com>
2020-06-05 15:23:06 +0200

    hola

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,8 +3,19 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
 
+    def attack():
+        return self.strength
+
+    def receiveDamage(self,damage):
+        self.health - damage
+
+        pass
+    
+        
 # Viking
 
 
