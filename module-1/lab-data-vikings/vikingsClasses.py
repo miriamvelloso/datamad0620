063206d523266e478b5685f51fd192850d721fed
miriamvelloso <62302676+miriamvelloso@users.noreply.github.com>
2020-06-05 19:08:35 +0200

    hola

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -7,29 +7,69 @@ class Soldier:
         self.health = health
         self.strength = strength
 
-    def attack():
+    def attack(self):
         return self.strength
 
     def receiveDamage(self,damage):
-        self.health - damage
+        self.health = - damage
 
-        pass
+        
     
         
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        super().__init__(health, strength)
 
+
+    def receiveDamage(self,damage):
+        self.health - damage
+        if health == 0:
+            return f"{self.name} has died in act of combat"
+        else:
+            return f"{self.name} has received {self.damage} points of damage"
+    
+    def battlecry(self):
+        return "Odin Owns You All!"
+    
+    
+    
 # Saxon
 
 
-class Saxon:
+class Saxon(Soldier):
+        def __init__(self):
+            super().__init__(health,strength)
+            self.health = health
+            self.strength = strength
+        
+        def receiveDamage(self,damage):
+            self.health - damage
+            if Saxon.health != 0:
+                return f "A Saxon has received {self.damage} points of damage"
+            else:
+                return f "A Saxon has died in combat"
+    
+
     pass
 
 # War
 
 
 class War:
+    def __init__(self): 
+        vikingArmy = []
+        saxonArmy = []
+
+    def addViking():
+        self.vikingArmy + 1 
+    def addSaxon():
+        self.vikingArmy +1
+    def vikingAttack():
+    def saxonAttack():
+    def showStatus():
+
     pass
