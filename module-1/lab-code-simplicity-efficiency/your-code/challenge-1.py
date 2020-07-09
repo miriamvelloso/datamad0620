ade4d880d45f726ab1748d50f89c528949430e8f
miriamvelloso <62302676+miriamvelloso@users.noreply.github.com>
2020-07-09 18:56:36 +0200

    cambios

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-1.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-1.py
@@ -5,14 +5,14 @@ def operation():
     a = int(input('Please choose your first number (zero to five): '))
     b = input('What do you want to do? plus or minus: ')
     c = int(input('Please choose your second number (zero to five): '))
-    if a not in range(0,6) or c not in range(0,6):
+    if a not in range(0,6) or c not in range(0,6) or b is not ('plus','minus'):
         print("I am not able to answer this question. Check your input.")
     else:  
         if b == 'plus':
             print(a+c)
         if b == 'minus':
             print(a-c)
-    print("Thanks for using this calculator, goodbye :)")
+        print("Thanks for using this calculator, goodbye :)")
 
     
 operation()
--- module-1/lab-code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-3.py
@@ -13,18 +13,8 @@ Refactor the code based on what you have learned about code simplicity and effic
 """
 
 def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                  solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+    return (max[x, y, z] for x in range(5, X) for y in range(4,X) for z in range(3,X) if (x*x==y*y+z*z))
+    X = (input("What is the maximal length of the triangle side? Enter a number: ")
+    print("The longest side possible is " + str(my_function(int(X))))
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
-
-print("The longest side possible is " + str(my_function(int(X))))
+my_function(X)
