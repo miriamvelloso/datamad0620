59c68181367595a1d74bbefe9c5a112215aec9d4
miriamvelloso <62302676+miriamvelloso@users.noreply.github.com>
2020-07-10 10:33:57 +0200

    hola

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-1.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-1.py
@@ -5,16 +5,26 @@ def operation():
     a = int(input('Please choose your first number (zero to five): '))
     b = input('What do you want to do? plus or minus: ')
     c = int(input('Please choose your second number (zero to five): '))
-    if a not in range(0,6) or c not in range(0,6) or b is not ('plus','minus'):
+    numbers_ = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    operation_ = ["plus", "minus"]
+
+   results= {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5}
+
+    if (not a in numbers_) or (not c in numbers_) or (not b in operation_):
         print("I am not able to answer this question. Check your input.")
     else:  
         if b == 'plus':
-            print(a+c)
+            print(a, b, c, "equals", numbers_[(results[a] + results[c])])
         if b == 'minus':
-            print(a-c)
+            print(a, b, c, "equals", numbers_[(results[a] - results[c])])
         print("Thanks for using this calculator, goodbye :)")
 
     
 operation()
 
- 
